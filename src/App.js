55daf9537daf91c73e199a757fb898dfd36977b4
KP <kpinc425@gmail.com>
2022-11-29 20:02:19 -0800

    Implement beginning of click indications, add simple dynamic styling

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
 
   const [pageDefaultTitle, setPageDefaultTitle] = useState(null);
 
+
   useEffect(() => {
     // Had to put async function inside useEffect
     const getDBTitle = async () => {
@@ -31,32 +32,53 @@ function App() {
     getDBTitle();
   }, [])
 
+  const handleImgClick = (e) => {
+    console.log("click");
+    const y = e.pageY;
+    const x = e.pageX;
+
+    console.log({x, y});
+    console.log(x);
+    console.log(y);
+
+    // const img = document.querySelector('.hiddenObjectImage');
+    const newCircle = document.createElement('div');
+    newCircle.classList.add('clickCircle');
+    newCircle.style.left = `${x - 12}px`;
+    newCircle.style.top = `${y - 32}px`;
+
+    console.log(newCircle.style.top);
+    console.log(newCircle.style.left);
+
+    document.querySelector('.hiddenObjectImage').appendChild(newCircle);
+
+
+  }
 
 
   return (
     <div className="App">
       <h1>{ pageDefaultTitle || "Find the Hidden Object!" }</h1> 
       <ControlBoard />
-      <StoryBoard />
+      <StoryBoard handleImgClick={ handleImgClick }/>
     </div>
   );
 }
 
-const StoryBoard = () => {
+const StoryBoard = ({ handleImgClick }) => {
   return (
     <div className='storyBoard backWaldoStyle'>
-      <div className='imagesContainer flex'>
-        <HiddenObjectImage />
-      </div>
+
+        <HiddenObjectImage handleImgClick={ handleImgClick } />
+
     </div>
   )
 }
 
-const HiddenObjectImage = () => {
+const HiddenObjectImage = ({ handleImgClick }) => {
   return (
-    <div className='hiddenObjectImage backWaldoStyle'>
-      <h1>Needs Image background</h1>
-      
+    <div className='hiddenObjectImage backWaldoStyle' onClick={ handleImgClick }>
+
     </div>
   )
 }
@@ -74,7 +96,7 @@ const CharacterBox = () => {
   return (
     <div className='charBox backWaldoStyle'>
       <h3>Who's Missing?</h3>
-      <div className='flex'>
+      <div className='flexButtons'>
         <div id='char1Avatar' className='circleStyle'></div>
         <div id='char2Avatar' className='circleStyle'></div>
         <div id='char3Avatar' className='circleStyle'></div>
@@ -86,7 +108,7 @@ const CharacterBox = () => {
 const AppControls = () => {
   return (
     <div className='appControls flex backWaldoStyle'>
-      <div className="flex">
+      <div className="flexButtons">
         <button className='circleStyle'>Start</button>
         <button className='circleStyle'>End</button>
         <button className='circleStyle'>Gallery</button>
